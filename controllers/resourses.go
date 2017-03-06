53dbf84d666e7c7bd69bd03dc196e29e56d08d95
gduryhk <guillaume@dury.pro>
2017-03-06 10:02:40 +0800

    Adding all the source files

---
+++ controllers/resourses.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"github.com/gduryhk/cinema-movies/models"
+)
+
+type (
+	// For Get - /movies
+	MoviesResource struct {
+		Data []models.Movie `json:"data"`
+	}
+	// For Post/Put - /movies
+	MovieResource struct {
+		Data models.Movie `json:"data"`
+	}
+)
